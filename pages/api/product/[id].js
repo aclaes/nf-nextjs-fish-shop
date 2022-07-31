6c171a6639d402c759c923271d5f7d3ef366441c
Alexander Claes <hello@aclaes.com>
2022-07-31 19:21:05 +0200

    Add API and form to edit product

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -1,4 +1,5 @@
 import { dbConnect } from '../../../src/dbConnect';
+import Product from '../../../src/model/productModel';
 import { getProductById } from '../../../src/services/productsService';
 
 export default async function handler(request, response) {
@@ -9,5 +10,9 @@ export default async function handler(request, response) {
   if (request.method === 'GET') {
     const product = await getProductById(id);
     response.status(200).json(product);
+  } else if (request.method === 'PUT') {
+    const data = JSON.parse(request.body);
+    await Product.findByIdAndUpdate(id, data);
+    response.status(200).json({ message: 'product updated' });
   }
 }
