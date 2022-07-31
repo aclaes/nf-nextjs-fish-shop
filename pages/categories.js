9391fe4d492cabd87b7716b349017208b6dc4b14
Alexander Claes <hello@aclaes.com>
2022-07-31 13:49:17 +0200

    Add routes for pages

---
+++ pages/categories.js
@@ -0,0 +1,8 @@
+export default function Categories() {
+  return (
+    <>
+      <h1>Kategorien</h1>
+      <p>Liste aller Kategorien</p>
+    </>
+  );
+}
---
+++ pages/products.js
@@ -0,0 +1,8 @@
+export default function Products() {
+  return (
+    <>
+      <h1>Produkte</h1>
+      <p>Liste aller Produkte</p>
+    </>
+  );
+}
